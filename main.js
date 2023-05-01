0030c846c2b7ced2ed149c6bed418acdcdd7ecea
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-01 13:42:46 +0300

    Add generate book card function

--- main.js
+++ main.js
@@ -36,3 +36,27 @@ function addBook() {
   }
   return null;
 }
+
+function generateBookCard(bookObj)
+{
+    const bookCard=document.createElement('li');
+    const bookTitle=document.createElement('p');
+    const bookAuthor=document.createElement('p')
+    const removeBtn=document.createElement('button');
+
+    bookCard.classList.add('book-card','mb-1');
+    bookTitle.classList.add('book-card-text');
+    bookAuthor.classList.add('book-card-text');
+    removeBtn.classList.add('remove-book','mb-05')
+
+    const { title, author } = bookObj;
+    bookTitle.textContent=title;
+    bookAuthor.textContent=author;
+    removeBtn.textContent='Remove'
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(removeBtn);
+    return bookCard;
+}
+
