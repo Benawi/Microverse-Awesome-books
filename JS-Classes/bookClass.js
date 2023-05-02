78da34d9cdf26101edfe4ca491083c103d061fc7
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-02 12:15:13 +0300

    Add book adder method

--- JS-Classes/bookClass.js
+++ JS-Classes/bookClass.js
@@ -1,14 +1,37 @@
-export default class BookClass{
-    constructor(){
-        this.BOOKS_DATA=JSON.parse(localStorage.getItem('book_data')) || [];
-    }
-    setStorage(){
-        localStorage.setItem('book_data',JSON.stringify(this.BOOKS_DATA));
-        
-    }
-    list(){
-        return this.BOOK_DATA;
-    }
+export default class BookClass {
+  constructor() {
+    this.BOOKS_DATA = JSON.parse(localStorage.getItem("book_data")) || [];
+  }
+  setStorage() {
+    localStorage.setItem("book_data", JSON.stringify(this.BOOKS_DATA));
+  }
+  list() {
+    return this.BOOK_DATA;
+  }
 
-   
+  addBooks() {
+    const bookTitle = document.getElementById("book-title");
+    const bookAuthor = document.getElementById("book-author");
+    const bookObj = {};
+
+    let exists = false;
+
+    for (let i = 0; i < BOOKS_DATA.length; i += 1) {
+      if (
+        BOOKS_DATA[i].title === bookTitle.value &&
+        BOOKS_DATA[i].author === bookAuthor.value
+      ) {
+        exists = true;
+      }
+    }
+    if (!exists) {
+      bookObj.title = bookTitle.value;
+      bookObj.author = bookAuthor.value;
+      bookTitle.value='';
+      bookAuthor.value='';
+    }
+   this.setStorage();
+  }
+
+  
 }
