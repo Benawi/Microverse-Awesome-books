9f6c9b90d9356a3faa2722a120a2c3c22fff1a42
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-03 10:36:04 +0300

    Add generate method to load local storage during refresh page

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-import AdderRemovebooks from './JS-Classes/AdderRemovebooks.js';
-import ListBooks from './JS-Classes/listBook.js';
+import AdderRemovebooks from "./JS-Classes/AdderRemovebooks.js";
+import ListBooks from "./JS-Classes/listBook.js";
 
 const adderRemovebooks = new AdderRemovebooks();
 const listBooks = new ListBooks();
-const form = document.querySelector('form');
-
-document.addEventListener('click', (event) => {
-  if (event.target && event.target.className.includes('remove-book')) {
+const form = document.querySelector("form");
+listBooks.generate(adderRemovebooks.list());
+document.addEventListener("click", (event) => {
+  if (event.target && event.target.className.includes("remove-book")) {
     adderRemovebooks.removeBook(event.target);
     listBooks.generate(adderRemovebooks.list());
   }
 });
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   adderRemovebooks.addBooks();
   listBooks.generate(adderRemovebooks.list());
   event.preventDefault();
