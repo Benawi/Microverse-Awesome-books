bcb473c53c9e0fd9b09d68b218340355ac896bbf
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-03 16:59:10 +0300

    import navigation class and update list generate method

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-import AdderRemovebooks from './JS-Classes/AdderRemovebooks.js';
-import ListBooks from './JS-Classes/listBook.js';
+import AdderRemovebooks from "./JS-Classes/AdderRemovebooks.js";
+import ListBooks from "./JS-Classes/listBook.js";
+import Navigation from "./JS-Classes/Navigation.js";
 
 const adderRemovebooks = new AdderRemovebooks();
 const listBooks = new ListBooks();
-const form = document.querySelector('form');
-listBooks.generate(adderRemovebooks.list());
-document.addEventListener('click', (event) => {
-  if (event.target && event.target.className.includes('remove-book')) {
+const nav = new Navigation();
+//const datetime = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+const datetime = new Date();
+
+const dateString = datetime.toLocaleDateString();
+const timeString = datetime.toLocaleTimeString();
+const dateTimeString = `${dateString} ${timeString}`;
+
+const dateTimeElement = document.getElementById("date-time");
+dateTimeElement.innerHTML = dateTimeString;
+const form = document.querySelector("form");
+
+listBooks.generate(adderRemovebooks.list(), datetime);
+nav.navigation.addEventListener("click", (event) => {
+  if (event.target.id === "nav-home") {
+    nav.sectionShow(document.getElementById("nav-list"));
+  } else if (event.target.nodeName === "A") {
+    nav.sectionShow(event.target);
+  }
+});
+document.addEventListener("click", (event) => {
+  if (event.target && event.target.className.includes("remove-book")) {
     adderRemovebooks.removeBook(event.target);
-    listBooks.generate(adderRemovebooks.list());
+    listBooks.generate(adderRemovebooks.list(), datetime);
   }
 });
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   adderRemovebooks.addBooks();
   listBooks.generate(adderRemovebooks.list());
   event.preventDefault();
