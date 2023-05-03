bcd1c5402d7842c8ca6b8813198d14d6aa923aae
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-03 15:13:38 +0300

    update link navigation method in navigation class

--- JS-Classes/Navigation.js
+++ JS-Classes/Navigation.js
@@ -13,6 +13,23 @@ export default class Navigation {
       link.setAttribute("class", "text-decoration-none fs-6 fw-bold px-1");
     }
 
-   
-
+    link.parentElement.parentElement
+      .querySelectorAll("a")
+      .forEach((element) => {
+        if (element !== link) {
+          if (element.className.includes("links")) {
+            element.setAttribute(
+              "class",
+              "links text-decoration-none text-dark fs-6 fw-bold px-2"
+            );
+          } else {
+            element.setAttribute(
+              "class",
+              "text-decoration-none text-dark fs-6 fw-bold px-1"
+            );
+          }
+        }
+      });
+  }
+  
 }
