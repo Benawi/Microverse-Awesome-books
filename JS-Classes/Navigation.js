fde9cea660de5301abb4aa4834774d8e4a45ae07
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-03 15:12:58 +0300

    Add link navigation method in navigation class

--- JS-Classes/Navigation.js
+++ JS-Classes/Navigation.js
@@ -3,5 +3,16 @@ export default class Navigation {
     this.navigation = document.querySelector("nav");
   }
 
-  
+  linkStyle(link) {
+    if (link.className.includes("links")) {
+      link.setAttribute(
+        "class",
+        "links text-decoration-none fs-6 fw-bold px-2"
+      );
+    } else {
+      link.setAttribute("class", "text-decoration-none fs-6 fw-bold px-1");
+    }
+
+   
+
 }
