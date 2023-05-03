9d6652dfbc09b24cd0a819a9f12a89943f414b6a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-05-03 16:55:25 +0300

    Add Navigation link and footer section

--- JS-Classes/Navigation.js
+++ JS-Classes/Navigation.js
@@ -1,49 +1,49 @@
 export default class Navigation {
   constructor() {
-    this.navigation = document.querySelector("nav");
+    this.navigation = document.querySelector('nav');
   }
 
   linkStyle(link) {
-    if (link.className.includes("links")) {
+    if (link.className.includes('links')) {
       link.setAttribute(
-        "class",
-        "links text-decoration-none fs-6 fw-bold px-2"
+        'class',
+        'links text-decoration-none fs-6 fw-bold px-2'
       );
     } else {
-      link.setAttribute("class", "text-decoration-none fs-6 fw-bold px-1");
+      link.setAttribute('class', 'text-decoration-none fs-6 fw-bold px-1');
     }
 
     link.parentElement.parentElement
-      .querySelectorAll("a")
+      .querySelectorAll('a')
       .forEach((element) => {
         if (element !== link) {
-          if (element.className.includes("links")) {
+          if (element.className.includes('links')) {
             element.setAttribute(
-              "class",
-              "links text-decoration-none text-dark fs-6 fw-bold px-2"
+              'class',
+              'links text-decoration-none text-dark fs-6 fw-bold px-2'
             );
           } else {
             element.setAttribute(
-              "class",
-              "text-decoration-none text-dark fs-6 fw-bold px-1"
+              'class',
+              'text-decoration-none text-dark fs-6 fw-bold px-1'
             );
           }
         }
       });
   }
   sectionChange(section) {
-    section.classList.remove("d-none");
-    section.parentElement.querySelectorAll("section").forEach((element) => {
-      if (element !== section && !element.className.includes("d-none")) {
-        element.classList.add("d-none");
+    section.classList.remove('d-none');
+    section.parentElement.querySelectorAll('section').forEach((element) => {
+      if (element !== section && !element.className.includes('d-none')) {
+        element.classList.add('d-none');
       }
     });
   }
 sectionShow(linkNav){
-    this.linkStyle(linkNav);
-    const sectionId='section-${linkNav.id.slice(4)}';
-    const section=document.getElementById(sectionId);
-    this.sectionChange(section);
+  this.linkStyle(linkNav);
+  const sectionId = `section-${linkNav.id.slice(4)}`;
+  const section = document.getElementById(sectionId);
+  this.sectionChange(section);
 }
 
 }
